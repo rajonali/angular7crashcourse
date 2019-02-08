b668d3d335545bf4e8a729e4e399ffb692fb38ee
Rajon Ali <rajonali44492@icloud.com>
2019-02-08 17:49:59 -0600

    Added styling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,16 @@
 import {Component, OnInit} from '@angular/core';
+import {DataService} from '../data.service';
 
 @Component({selector: 'app-home', templateUrl: './home.component.html', styleUrls: ['./home.component.scss']})
 export class HomeComponent implements OnInit {
-    h1Style : boolean = false;
-    constructor() {}
-    
-    firstClick() {
-      this.h1Style= true;
+    users : Object;
+    constructor(private data : DataService) {}
+
+    ngOnInit() {
+      this.data.getUsers().subscribe(data => { 
+        this.users = data
+        console.log(this.users)
+      })
     }
-    ngOnInit() {}
 
 }
